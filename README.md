b793e6baf4231a2913cfe4271e8cdc413f82905e
Dylan <dylanchew3@gmail.com>
2022-11-23 15:15:38 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ICT2104_Car
+
+This is just a link to the repo, code will be uploaded later.
--- uart_pc_echo_12mhz_brclk/uart_pc_echo_12mhz_brclk/Init/printf.h
+++
@@ -1,9 +0,0 @@
-#ifndef PRINTF_H_
-#define PRINTF_H_
-
-void initprintf(void);
-void Uartputs(uint32_t moduleInstance, char *s);
-void Uartputc(uint32_t moduleInstance, unsigned b);
-void Uartprintf(uint32_t moduleInstance, char *, ...);
-
-#endif /* PRINTF_H_ */
